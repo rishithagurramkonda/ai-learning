f4896cdc799834c28301fd6fac8960f8dc859494
rishithagurramkonda <rishithagurramkonda@gmail.com>
2026-02-25 15:59:57 +0530

    pythonBasics

---
+++ Hello.py
@@ -0,0 +1,19 @@
+print("Hello, AI World!")
+
+def userName(name):
+    if(name == 'Rishi'):
+        print("Welcome Rishi")
+    else:
+        print(name)
+
+userName('Uma')
+
+skills = ["React", "Python", "AI"]
+
+profile = {
+    "name": "Rishitha",
+    "role": "Software Engineer"
+}
+
+print(skills)
+print(profile["role"])
